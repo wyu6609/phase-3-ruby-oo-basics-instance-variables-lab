bb64ae895a48ff0199eda0ea0e9adf55f9ccc0f0
William Yu <88666309+wyu6609@users.noreply.github.com>
2022-03-15 11:11:15 -0400

    completed

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
 class Dog
+  # setter method
+  def name=(value)
+    @name = value
+  end
 
+  # getter method
+  def name
+    # access the data from the @name instance variable and return it
+    @name
+  end
 end
+
+odie = Dog.new
+odie.name = 'Odie'
+
+# call the Dog#name method
+puts odie.name
+# "Odie"
